4dbeb2e29f09ee0228ed7c92e4a7e3dc07dfbd22
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2022-06-08 10:41:19 +0600

    Add image

--- README.md
+++ README.md
@@ -7,7 +7,10 @@
     It is use the stack[LIFO] to solve the problem.
     
     Space Efficient : Recursion : NOT , Iteartive : YES
-    Time Efficient : Recursion : YES , Iteartive : NOT
+    Time Efficient : Recursion : NOT , Iteartive : YES
     Easy to implement : Recursion : YES , Iteartive : NOT
+    
+    ![img.png](img.png)
    
   problem practive: https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1439/
+ 
---
+++ src/recursion/Factorial.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        Factorial fac = new Factorial();
+        var rec = fac.factorial(-2);
+
+        System.out.println(rec);
+
+    }
+
+    public int factorial(int n){
+        if(n<0) // not work on negative number
+            return -1;
+        if(n==0 || n==1) // base case
+            return 1;
+        return n*(factorial(n-1));
+    }
+}
