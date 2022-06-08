612b6a789c6a34ae2b74947f1daf518a0d90d625
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2022-06-08 10:21:08 +0600

    dzsf

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 1. Recursion :
     Recusion is a technique to solve a problem by breaking it down into smaller and smaller sub-problems.
     It is use the stack[LIFO] to solve the problem.
+    
+    Space Efficient : Recursion : NOT , Iteartive : YES
+    Time Efficient : Recursion : YES , Iteartive : NOT
+    Easy to implement : Recursion : YES , Iteartive : NOT
    
   problem practive: https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1439/
---
+++ src/recursion/Power.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class Power {
+
+    public static void main(String[] args) {
+        System.out.println(powerOfTwo(3));
+    }
+
+    static int powerOfTwo(int n){
+        if(n == 0){
+            return 1;
+        }
+        return 2 * powerOfTwo(n-1);
+    }
+
+    static int powerOfTwo2(int n){
+        var i=0;
+        var power = 1;
+        while(i < n){
+            power = power * 2;
+            i++;
+        }
+        return power;
+    }
+}
